645cde97657eebe8b8ef9e4ed6501b5cc8abbc04
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2022-10-17 23:51:07 -0400

    login frontend layout

--- app/src/main/java/com/example/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -9,20 +9,28 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonSingIn, buttonRegister;
+    Button buttonSignIn, buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonSingIn = findViewById(R.id.btnSignIn);
+        buttonSignIn = findViewById(R.id.btnSignIn);
         buttonRegister = findViewById(R.id.btnRegister);
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
