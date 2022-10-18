eefa353138010c9437e3391f8761471033d29ded
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2022-10-18 07:02:13 -0400

    logout user

--- app/src/main/java/com/example/restaurantapp/HomeActivity.java
+++ app/src/main/java/com/example/restaurantapp/HomeActivity.java
@@ -2,13 +2,31 @@ package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        buttonLogout = findViewById(R.id.sendLogout);
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
